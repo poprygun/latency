5883ef156f08b7c9e35d628766687f1193253a96
Alex Shumilov <ashumilov@pivotal.io>
2019-11-04 06:59:39 -0500

    Add HttpClient so Http Pooling Connection Factory is used.

--- client/src/main/java/io/microsamples/latency/client/AppConfiguration.java
+++ client/src/main/java/io/microsamples/latency/client/AppConfiguration.java
@@ -1,5 +1,6 @@
 package io.microsamples.latency.client;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfiguration {
 
     @Bean
-    RestTemplate getRestTemplate(){
-        return new RestTemplate();
+    RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
